c093e390b1ac8e6bf6b916ff11fc8ea03b986b40
greatSumini <greatSumini@gmail.com>
2025-09-03 04:14:08 +0900

    chore: update .gitignore

--- dist/plugins/IPlugin.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=IPlugin.js.map
--- dist/utils/tokens.js
+++
@@ -1,7 +0,0 @@
-export function estimateTokenUsage(text, options) {
-    // 간단한 휴리스틱: 토큰 ~= 문자수 / 4
-    const tokens = Math.ceil((text?.length || 0) / 4);
-    const costUSD = (tokens / 1_000_000) * options.pricePerMTokUSD;
-    return { tokens, costUSD };
-}
-//# sourceMappingURL=tokens.js.map
